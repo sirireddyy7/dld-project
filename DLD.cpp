716333a87ab7784fafbbab0e709f5d2f568feda2
sirireddy <samulya2006@gmail.com>
2026-09-15 13:24:03 +0530

    Update DLD program

--- DLD.cpp
+++ DLD.cpp
@@ -3,19 +3,26 @@
 #include <string>
 
 using namespace std;
+
 int variables;
 int rows;
 int columns;
+
 vector<vector<int>> kmap;
+
 vector<unsigned long long> groups;
 vector<int> groupLiterals;
+
 vector<int> bestAnswer;
 int bestLiterals = 1000;
+
 int gray(int position)
 {
     int grayCode[8] = {0, 1, 3, 2, 6, 7, 5, 4};
+
     return grayCode[position];
 }
+
 int getMinterm(int row, int column)
 {
     int rowBits = variables / 2;
@@ -29,42 +36,78 @@ int getMinterm(int row, int column)
     return minterm;
 }
 
+int findLiterals(unsigned long long group)
+{
+    int count = 0;
+
+    for (int bit = 0; bit < variables; bit++)
+    {
+        bool hasZero = false;
+        bool hasOne = false;
+
+        for (int minterm = 0; minterm < (1 << variables); minterm++)
+        {
+            if (group & (1ULL << minterm))
+            {
+                int value = (minterm >> (variables - 1 - bit)) & 1;
+
+                if (value == 0)
+                    hasZero = true;
+                else
+                    hasOne = true;
+            }
+        }
+
+        if (!(hasZero && hasOne))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
 void findGroups()
 {
     groups.clear();
     groupLiterals.clear();
 
+    // Try different group heights
     for (int height = 1; height <= rows; height = height * 2)
     {
+        // Try different group widths
         for (int width = 1; width <= columns; width = width * 2)
         {
+            // Try every starting row
             for (int startRow = 0; startRow < rows; startRow++)
             {
+                // Try every starting column
                 for (int startColumn = 0; startColumn < columns; startColumn++)
                 {
                     bool valid = true;
 
                     unsigned long long group = 0;
 
-                    // Check every cell inside the possible group
+                    // Check every cell inside the group
                     for (int r = 0; r < height; r++)
                     {
                         for (int c = 0; c < width; c++)
                         {
-                            // % allows K-map wrap-around
+                            // % allows wrap-around
                             int row = (startRow + r) % rows;
                             int column = (startColumn + c) % columns;
 
-                            // If any cell is 0, group is invalid
+                            // If any cell is 0,
+                            // this group is not valid
                             if (kmap[row][column] == 0)
                             {
                                 valid = false;
                             }
 
-                            // Find minterm number
+                            // Find minterm of this cell
                             int minterm = getMinterm(row, column);
 
-                            // Remember that this minterm belongs to group
+                            // Store this minterm in the group
                             group = group | (1ULL << minterm);
                         }
                     }
@@ -74,6 +117,8 @@ void findGroups()
                     {
                         bool alreadyExists = false;
 
+                        // Check whether this group
+                        // was already found
                         for (int i = 0; i < (int)groups.size(); i++)
                         {
                             if (groups[i] == group)
@@ -86,6 +131,7 @@ void findGroups()
                         if (!alreadyExists)
                         {
                             groups.push_back(group);
+
                             groupLiterals.push_back(
                                 findLiterals(group)
                             );
@@ -95,42 +141,6 @@ void findGroups()
             }
         }
     }
-}
-
-int findLiterals(unsigned long long group)
-{
-    int count = 0;
-
-    // Check all variables
-    for (int bit = 0; bit < variables; bit++)
-    {
-        bool hasZero = false;
-        bool hasOne = false;
-
-        // Check all minterms
-        for (int minterm = 0; minterm < (1 << variables); minterm++)
-        {
-            // Is this minterm inside the group?
-            if (group & (1ULL << minterm))
-            {
-                int value = (minterm >> (variables - 1 - bit)) & 1;
-
-                if (value == 0)
-                    hasZero = true;
-                else
-                    hasOne = true;
-            }
-        }
-
-        // If variable does not change,
-        // it remains in the expression
-        if (!(hasZero && hasOne))
-        {
-            count++;
-        }
-    }
-
-    return count;
 }
 
 string getExpression(unsigned long long group)
@@ -144,12 +154,12 @@ string getExpression(unsigned long long group)
         bool hasZero = false;
         bool hasOne = false;
 
-        // Check every minterm in this group
         for (int minterm = 0; minterm < (1 << variables); minterm++)
         {
             if (group & (1ULL << minterm))
             {
-                int value = (minterm >> (variables - 1 - bit)) & 1;
+                int value =
+                    (minterm >> (variables - 1 - bit)) & 1;
 
                 if (value == 0)
                     hasZero = true;
@@ -158,21 +168,25 @@ string getExpression(unsigned long long group)
             }
         }
 
-        // Variable remains only if it does not change
+        // Variable is always 0
         if (hasZero && !hasOne)
         {
             answer += variableNames[bit];
             answer += "'";
         }
+
+        // Variable is always 1
         else if (!hasZero && hasOne)
         {
             answer += variableNames[bit];
         }
     }
 
-    // If no variable remains, group represents 1
+    // No variable remains
     if (answer == "")
+    {
         answer = "1";
+    }
 
     return answer;
 }
@@ -189,13 +203,15 @@ unsigned long long getAllOnes()
             {
                 int minterm = getMinterm(row, column);
 
-                allOnes = allOnes | (1ULL << minterm);
+                allOnes =
+                    allOnes | (1ULL << minterm);
             }
         }
     }
 
     return allOnes;
 }
+
 
 void findBestAnswer(
     unsigned long long covered,
@@ -205,64 +221,88 @@ void findBestAnswer(
 {
     unsigned long long allOnes = getAllOnes();
 
-    // If all 1s are covered
+
+    // If every 1 is covered
     if (covered == allOnes)
     {
         if (totalLiterals < bestLiterals)
         {
             bestLiterals = totalLiterals;
+
             bestAnswer = selected;
         }
 
         return;
     }
 
-    // No need to continue if already worse
+
+    // Stop if this answer is already worse
     if (totalLiterals >= bestLiterals)
     {
         return;
     }
 
-    // Find the first uncovered 1
+
+    // Find first uncovered 1
     int uncoveredMinterm = -1;
 
-    for (int minterm = 0; minterm < (1 << variables); minterm++)
+    for (int minterm = 0;
+         minterm < (1 << variables);
+         minterm++)
     {
         if (allOnes & (1ULL << minterm))
         {
             if (!(covered & (1ULL << minterm)))
             {
                 uncoveredMinterm = minterm;
+
                 break;
             }
         }
     }
 
+
     // Try every group
-    for (int i = 0; i < (int)groups.size(); i++)
+    for (int i = 0;
+         i < (int)groups.size();
+         i++)
     {
-        // Does this group cover the uncovered 1?
+        // Does this group cover
+        // the uncovered 1?
         if (groups[i] & (1ULL << uncoveredMinterm))
         {
-            // Avoid selecting the same group again
             bool alreadySelected = false;
 
-            for (int j = 0; j < (int)selected.size(); j++)
+
+            // Check whether group is
+            // already selected
+            for (int j = 0;
+                 j < (int)selected.size();
+                 j++)
             {
                 if (selected[j] == i)
                 {
                     alreadySelected = true;
+
                     break;
                 }
             }
 
-            if (alreadySelected)
-                continue;
 
+            if (alreadySelected)
+            {
+                continue;
+            }
+
+
+            // Select this group
             selected.push_back(i);
 
+
+            // Add the newly covered 1s
             unsigned long long newCovered =
                 covered | groups[i];
+
 
             findBestAnswer(
                 newCovered,
@@ -270,31 +310,43 @@ void findBestAnswer(
                 totalLiterals + groupLiterals[i]
             );
 
+
             selected.pop_back();
         }
     }
 }
 
+
 void printAnswer()
 {
     cout << "\nMinimized Boolean Expression:\n";
 
-    for (int i = 0; i < (int)bestAnswer.size(); i++)
+    for (int i = 0;
+         i < (int)bestAnswer.size();
+         i++)
     {
         if (i > 0)
+        {
             cout << " + ";
+        }
 
-        cout << getExpression(groups[bestAnswer[i]]);
+        cout << getExpression(
+            groups[bestAnswer[i]]
+        );
     }
 
     cout << endl;
 }
 
+
 int main()
 {
     cout << "Enter number of variables (2, 3, 4 or 5): ";
+
     cin >> variables;
 
+
+    // Find K-map size
     if (variables == 2)
     {
         rows = 2;
@@ -318,6 +370,7 @@ int main()
     else
     {
         cout << "Only 2, 3, 4 and 5 variables are allowed.\n";
+
         return 0;
     }
 
@@ -338,7 +391,9 @@ int main()
             cin >> kmap[i][j];
         }
     }
+
     bool allZero = true;
+
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
@@ -349,13 +404,18 @@ int main()
             }
         }
     }
+
+
     if (allZero)
     {
         cout << "\nMinimized Boolean Expression:\n";
         cout << "0\n";
+
         return 0;
     }
+
     bool allOne = true;
+
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
@@ -366,15 +426,26 @@ int main()
             }
         }
     }
+
+
     if (allOne)
     {
         cout << "\nMinimized Boolean Expression:\n";
         cout << "1\n";
+
         return 0;
     }
+
     findGroups();
+
     vector<int> selected;
-    findBestAnswer(0, selected, 0);
+
+    findBestAnswer(
+        0,
+        selected,
+        0
+    );
+
     printAnswer();
     return 0;
 }
